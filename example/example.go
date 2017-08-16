ee3f797260f27b38d405da2cba153739488b9bec
as <as.utf8@gmail.com>
2017-08-15 21:28:33 -0700

    server example

---
+++ example/example.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"fmt"
+	"github.com/as/hub"
+	"github.com/as/text"
+)
+
+func main() {
+	a := os.Args
+	if len(a) < 2 {
+		log.Fatalln("usage: hubs.exe host:port")
+	}
+	buf := text.NewBuffer()
+	var (
+		data []byte
+		err error
+	)
+	for _, name := range []string{`\windows\system32\drivers\etc\hosts`, `/etc/hosts`, `/ndb/local`}{
+		data, err = ioutil.ReadFile(name)
+		if err == nil{
+			break
+		}
+	}
+	if err != nil {
+		log.Fatalln(fmt.Errorf("no hosts files\n"))
+	}
+	buf.Insert(data, 0)
+	hub := hub.NewHub(buf)
+	hub.Run()
+	hub.ListenAndServe(a[1])
+	select {}
+}
---
+++ wire/wire.go
@@ -0,0 +1,55 @@
+package wire
+
+import (
+	"fmt"
+
+	"github.com/as/text"
+)
+
+const (
+	Broadcast = 10
+	Reply     = 11
+)
+
+type Packet struct {
+	Id   int
+	Kind byte
+	RcId int
+	Data
+}
+
+type Note struct {
+	Ch int
+	Packet
+}
+
+type Data struct {
+	Q0, Q1 int64
+	N      int
+	Err    string
+	P      []byte
+}
+
+func StrToErr(str string) error{
+	if str == ""{
+		return nil
+	}
+	return fmt.Errorf("%s", str)
+}
+func Err(err error) string{
+	if err != nil{
+		return err.Error()
+	}
+	return ""
+}
+
+func (p Packet) String() string {
+	return fmt.Sprintf("packet %d: kind: '%c', rc: %d, dot: (%d:%d), n: %d, len(p): %d, err: %s\n",
+		p.Id, p.Kind, p.RcId, p.Q0, p.Q1, p.N, len(p.P), p.Err)
+}
+
+func PacketOk(Kind byte) bool {
+	return true
+	var valIdKind = []byte("IdswrR.blmELOCS")
+	return text.Any(Kind, valIdKind) != -1
+}
